74c1db201216c5e90fb82468b3d09835ed9c3fb4
shavel404 <shavel-96@yandex.com>
2020-12-24 09:55:33 +0300

    task1

---
+++ task1/image.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: image/jpg');
+
+
+readfile($_SERVER['DOCUMENT_ROOT'].'/picture.jpg');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ task1/index.php
@@ -0,0 +1,22 @@
+
+<img src = "image.php"/>
+<img src = "image.php"/>
+<img src = "image.php"/>
+<img src = "image.php"/>
+
+
+<script>
+
+  const imgs = document.querySelectorAll("img")
+  let n = 0;
+
+  for (let i = 0; i < imgs.length;i++){
+    if(imgs[i].src.match('image.php')) n++;
+  }
+
+  alert(n)
+
+</script>
+
+
+
